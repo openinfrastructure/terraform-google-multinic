89dda8659d1c9d76ec2d6a0dff6907d5ef2dc30d
Jeff McCune <jeff@openinfrastructure.co>
2019-08-26 17:01:21 -0700

    Use a dhcp exit hook to restore rt1
    Without this patch the two entries in rt1 are lost when dhclient
    reconfigures eth1.  This situation arises in at least two places:
    
     1. Race condition during startup, google-network-daemon executes
        concurrently with startup-script runner
     2. `systemctl restart google-network-daemon`.
    
    This patch fixes the problem by configuring
    `/etc/dhcp/dhclient-exit-hooks.d/vpc-link.sh` with the behavior of
    restoring rt1 when eth1 is plumbed.
    
    Note this is only tested for eth1, not for eth2 ... eth7.

--- files/ip-router-mig.sh
+++ files/ip-router-mig.sh
@@ -67,6 +67,14 @@ setup_sysctl() {
   echo 'net.ipv4.conf.all.forwarding=1'     >> "$sysctl_file"
   echo 'net.ipv4.conf.default.rp_filter=0'  >> "$sysctl_file"
   echo 'net.ipv4.conf.all.rp_filter=0'      >> "$sysctl_file"
+  echo 'net.ipv4.conf.eth0.rp_filter=0'     >> "$sysctl_file"
+  echo 'net.ipv4.conf.eth1.rp_filter=0'     >> "$sysctl_file"
+  echo 'net.ipv4.conf.eth2.rp_filter=0'     >> "$sysctl_file"
+  echo 'net.ipv4.conf.eth3.rp_filter=0'     >> "$sysctl_file"
+  echo 'net.ipv4.conf.eth4.rp_filter=0'     >> "$sysctl_file"
+  echo 'net.ipv4.conf.eth5.rp_filter=0'     >> "$sysctl_file"
+  echo 'net.ipv4.conf.eth6.rp_filter=0'     >> "$sysctl_file"
+  echo 'net.ipv4.conf.eth7.rp_filter=0'     >> "$sysctl_file"
   install -o 0 -g 0 -m 0644 "$sysctl_file" '/etc/sysctl.d/50-ip-router.conf'
   debug '# END # setup_sysctl() ...'
 
@@ -189,6 +197,7 @@ program_routes() {
 # need to configure policy routing to ensure that egress packets will leave
 # through the correct interface. In such cases, you must configure a separate
 # routing table for each network interface using policy routing.
+#
 configure_policy_routing() {
   local ary app_cidr ip gateway
   if ! grep -qx '1 rt1' /etc/iproute2/rt_tables; then
@@ -196,8 +205,13 @@ configure_policy_routing() {
   fi
   ip="$(stdlib::metadata_get -k instance/network-interfaces/1/ip)"
   gateway="$(stdlib::metadata_get -k instance/network-interfaces/1/gateway)"
+  # NOTE: dhclient clears out this
+  # Clear all rules associated with rt1 to prevent rules from building up
+  ip rule | grep 'table rt1' | cut -d: -f1 \
+    | xargs -n1 sudo ip rule del pref
   cmd ip route add "${APP_SUBNET_CIDR}" src "${ip}" dev eth1 table rt1
   cmd ip route add default via "${gateway}" dev eth1 table rt1
+  # NOTE: These route rules are not cleared by dhclient, they persist.
   cmd ip rule add from "${ip}/32" table rt1
   cmd ip rule add to "${ip}/32" table rt1
 
@@ -205,6 +219,10 @@ configure_policy_routing() {
   for app_cidr in "${ary[@]}"; do
     cmd ip rule add to "${app_cidr}" table rt1
   done
+  # Flush the route cache as per the iproute2 manual
+  cmd ip route flush cache
+  info "Finished configuring policy routing rules"
+  cmd ip rule
   return 0
 }
 
@@ -242,6 +260,11 @@ delete_stale_routes() {
 }
 
 main() {
+  if ! save_config; then
+    error "Failed to save configuration to /etc/startup-script-config, needed for dhclient hooks"
+    exit 1
+  fi
+
   if ! setup_sysctl; then
     error "Failed to configure ip forwarding via sysctl, aborting."
     exit 1
@@ -252,6 +275,15 @@ main() {
     exit 3
   fi
   info "Configured Policy Routing as per https://cloud.google.com/vpc/docs/create-use-multiple-interfaces#configuring_policy_routing"
+
+  if ! configure_dhclient_exit_hook; then
+    error "Failed to install /etc/dhcp/dhclient-exit-hooks.d/vpc-link.sh"
+    exit 1
+  fi
+  info "Installed /etc/dhcp/dhclient-exit-hooks.d/vpc-link.sh to restore policy routing on new DHCP lease"
+
+  # Restart google-network-daemon to avoid race condition with dhclient eth1
+  systemctl restart google-network-daemon
 
   if ! delete_stale_routes; then
     error "Failed to delete stale routes, aborting"
@@ -291,6 +323,83 @@ load_stdlib() {
   fi
   # shellcheck disable=1090
   source "${tmpfile}"
+}
+
+# Saves the startup-script-config metadata key to /etc/startup-script-config so
+# that /etc/dhcp/dhclient-exit-hooks.d/vpc-link.sh can load the configuration
+# from the local filesystem easily.
+save_config() {
+  local tmpfile ip gateway
+  tmpfile="$(mktemp)"
+  if ! curl --silent --fail -H 'Metadata-Flavor: Google' -o "${tmpfile}" \
+    http://metadata/computeMetadata/v1/instance/attributes/startup-script-config; then
+    error "Could not load config from metadata instance/attributes/startup-script-config"
+    return 1
+  fi
+  install -o 0 -g 0 -m 0644 "${tmpfile}" /etc/startup-script-config
+
+  # The gateway is not available in dhclient-exit-hooks.d because dhclient does
+  # not set routers when classless static routes are provided.
+  tmpfile="$(mktemp)"
+  gateway="$(stdlib::metadata_get -k instance/network-interfaces/1/gateway)"
+  echo "GATEWAY='${gateway}'" >> "${tmpfile}"
+  install -o 0 -g 0 -m 0644 "${tmpfile}" /etc/startup-script-config-eth1
+}
+
+# Configure dhclient exit hook to avoid race condition at startup and to ensure
+# ip routing configuration persists across restarts of the
+# google-network-daemon service.
+# See: https://github.com/openinfrastructure/terraform-google-vpc-link/issues/1
+# See: https://github.com/openinfrastructure/terraform-google-vpc-link/issues/2
+configure_dhclient_exit_hook() {
+  local tmpfile
+  tmpfile="$(mktemp)"
+  cat >"${tmpfile}" <<'EOF'
+#! /bin/bash
+#
+# NOTE: This script is sourced by dhclient-script instead of executed
+
+configure_policy_routing() {
+  # The startup-script should have configured this already, but in case not.
+  if ! grep -qx '1 rt1' /etc/iproute2/rt_tables; then
+    echo "1 rt1" >> /etc/iproute2/rt_tables
+  fi
+  # Get the GATEWAY for the interface becasue it is not provided by dhclient
+  # See: https://github.com/openinfrastructure/terraform-google-vpc-link/issues/2
+  # The startup-script is expected to write this file once on boot.
+  source /etc/startup-script-config-"${interface}"
+  # Get the APP_SUBNET_CIDR from the user provided configuration
+  source /etc/startup-script-config
+
+  logger -t vpc-link ip route add default via "${GATEWAY}" dev "${interface}" table rt1
+  ip route add default via "${GATEWAY}" dev "${interface}" table rt1
+  logger -t vpc-link ip route add "${APP_SUBNET_CIDR}" src "${new_ip_address}" dev "${interface}" table rt1
+  ip route add "${APP_SUBNET_CIDR}" src "${new_ip_address}" dev "${interface}" table rt1
+  logger -t vpc-link ip route flush cache
+  ip route flush cache
+  return 0
+}
+
+# Return true if the DHCP reason is valid to reconfigure routing.
+# BOUND, RENEW, REBIND, REBOOT
+valid_reason() {
+  local check
+  # Do nothing for eth0
+  [[ "${interface}" == "eth0" ]] && return 2
+  # For non eth0 interfaces, execute when a valid IP is obtained
+  for check in BOUND RENEW REBIND REBOOT; do
+    [[ "${reason}" == "${check}" ]] && return 0
+  done
+  return 1
+}
+
+case "${interface}" in
+  eth0) exit 0 ;;
+  eth*) valid_reason && configure_policy_routing ;;
+  *) exit 0 ;;
+esac
+EOF
+  install -o 0 -g 0 -m 0755 "${tmpfile}" /etc/dhcp/dhclient-exit-hooks.d/vpc-link.sh
 }
 
 # If the script is being executed directly, e.g. when running interactively,
